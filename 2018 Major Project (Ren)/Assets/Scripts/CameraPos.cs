7e9b7ba8b38bb5e513fbd34d64f4aab206ad13db
Pommanthebrit <warburmf@hotmail.com>
2018-10-02 10:38:11 +0800

    CameraPos

---
+++ 2018 Major Project (Ren)/Assets/Scripts/CameraPos.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPos : MonoBehaviour {
+    public GameObject Camera;
+    public GameObject Ren;
+    public GameObject Nabella;
+    public GameObject Midpoint;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        Midpoint.transform.position = (Ren.transform.position + Nabella.transform.position) / 2;
+        Camera.transform.LookAt(Midpoint.transform) ;
+    }
+}
