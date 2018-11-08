a1f14699280179a7564a518959edf7e285fd1195
SAE\Phollands <1008496@student.sae.edu.au>
2018-11-08 17:04:54 +0800

    Coding
    Coding

---
+++ 2018 Major Project (Ren)/Assets/Scripts/Buttons.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Buttons : MonoBehaviour {
+
+    public void Startbutton() {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Exit()
+    {
+       Application.Quit();
+    }
+
+    public void Controls()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ 2018 Major Project (Ren)/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour {
+
+    public bool paused = false;
+
+    [SerializeField]
+    GameObject pauseMenu;
+
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && paused == true)
+        {
+            if () {
+                Resume();
+            } else {
+                Pause();
+            }
+        }
+       
+	}
+
+    public void Resume() {
+        paused = false;
+        pauseMenu.SetActive(true);
+    }
+
+    public void Pause() {
+        paused = true;
+        pauseMenu.SetActive(false);
+
+    }
+}
